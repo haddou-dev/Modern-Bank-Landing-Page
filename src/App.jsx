0c2db3c207380c2ec817ee857708fe5ce6e01f40
mohamed haddou <haddou0mohamed@gmail.com>
2023-04-07 20:07:07 +0000

    first commit Create Navbar and make it responsive

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+/** @format */
+
+import React from "react";
+import styles from "./style";
+import {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+} from "./components";
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    {/* tha navBar Section */}
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+    {/* tha Hero Section */}
+    <div className={`bg-primary ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    {/* tha other Sections */}
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+  </div>
+);
+
+export default App;
