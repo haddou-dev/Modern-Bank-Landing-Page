a8d86324dd5f6242191b58ea0d3bdfdedc66ce17
mohamed haddou <haddou0mohamed@gmail.com>
2023-04-08 14:04:58 +0000

    add The Hero Section

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+/** @format */
 
-const GetStarted = () => {
-  return (
-    <div>GetStarted</div>
-  )
-}
+// import styles name classes from style.js file :
+import styles from "../style";
+// import images or icons by name from assets :
+import { arrowUp } from "../assets";
 
-export default GetStarted
+const GetStarted = () => (
+  <div
+    className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+    <div
+      className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
+      <div className={`${styles.flexStart} flex-row`}>
+        <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
+          <span className="text-gradient">Get</span>
+        </p>
+        <img
+          src={arrowUp}
+          alt="arrowUp"
+          className="w-[23px] h-[23px] object-contain"
+        />
+      </div>
+      <p className="font-poppins font-medium text-[18px] leading-[23px]">
+        <span className="text-gradient">Started</span>
+      </p>
+    </div>
+  </div>
+);
+
+export default GetStarted;
